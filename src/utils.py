477b4428176a9d89deddb6c92d20cf7e9e68be89
William Guo <willi.guo@gmail.com>
2024-10-23 16:21:12 -0400

    giga update

---
+++ src/utils.py
@@ -0,0 +1,271 @@
+# Function for creating a descriptive stats report for numerical data
+
+def numerical_dqr(df):
+  
+  # Select numerical columns
+  numerical = df.select_dtypes(include = ['int', 'Int64', 'float']).columns.tolist()
+  
+  # Data type
+  data_types = pd.DataFrame(
+    df[numerical].dtypes,
+    columns=['Data Type'])
+  
+  # Missing data
+  missing_data = pd.DataFrame(
+    df[numerical].isnull().sum(),
+    columns=['Missing Values'])
+  
+  # Unique values
+  unique_values = pd.DataFrame(
+    columns=['Unique Values'])
+  for row in list(df[numerical].columns.values):
+    unique_values.loc[row] = [df[numerical][row].nunique()]
+    
+  # Number of records
+  count_values = pd.DataFrame(
+    columns=['Records'])
+  for row in list(df[numerical].columns.values):
+      count_values.loc[row] = [df[numerical][row].count()]
+    
+  # Minimum value
+  maximum_values = pd.DataFrame(
+    columns=['Maximum Value'])
+  for row in list(df[numerical].columns.values):
+      maximum_values.loc[row] = [df[numerical][row].max()]
+    
+  # Maximum value
+  minimum_values = pd.DataFrame(
+    columns=['Minimum Value'])
+  for row in list(df[numerical].columns.values):
+      minimum_values.loc[row] = [df[numerical][row].min()]
+    
+  # Mean value
+  mean_values = pd.DataFrame(
+    columns=['Mean Value'])
+  for row in list(df[numerical].columns.values):
+      mean_values.loc[row] = [df[numerical][row].mean()]
+    
+  # First quartile
+  quartile_1 = pd.DataFrame(
+    columns=['1st Quartile'])
+  for row in list(df[numerical].columns.values):
+      quartile_1.loc[row] = [df[numerical][row].quantile(0.25)]
+    
+  # Median
+  median = pd.DataFrame(
+  columns=['Median'])
+  for row in list(df[numerical].columns.values):
+    median.loc[row] = [df[numerical][row].quantile(0.5)]
+    
+  # Third quartile
+  quartile_3 = pd.DataFrame(
+    columns=['3rd Quartile'])
+  for row in list(df[numerical].columns.values):
+    quartile_3.loc[row] = [df[numerical][row].quantile(0.75)]
+    
+  # Join columns 
+  dq_report_num = data_types.join(count_values).join(missing_data).join(unique_values).join(minimum_values).join(maximum_values).join(mean_values).join(quartile_1).join(median).join(quartile_3)
+  
+  # Percentage missing
+  dq_report_num['Missing %'] = (dq_report_num['Missing Values'] / len(df[numerical]) *100)
+  
+  # Change order of columns
+  dq_report_num = dq_report_num[['Data Type', 'Records', 'Unique Values', 'Missing Values', 'Missing %', 'Mean Value', 'Minimum Value', '1st Quartile', 'Median', '3rd Quartile', 'Maximum Value', ]]
+
+  # Round
+  dq_report_num[['Missing %', 'Mean Value', 'Minimum Value', '1st Quartile', 'Median', '3rd Quartile', 'Maximum Value']] =   dq_report_num[['Missing %', 'Mean Value', 'Minimum Value', '1st Quartile', 'Median', '3rd Quartile', 'Maximum Value']].round(1)
+  
+  # Return report
+  return(dq_report_num)
+
+
+# Custom function for creating a descriptive stats report for categorical data
+
+def categorical_dqr(df):
+
+  # Select categorical columns
+  categorical = df.select_dtypes(include = 'object').columns.tolist()
+
+  # Data type
+  data_types = pd.DataFrame(
+    df[categorical].dtypes,
+    columns=['Data Type'])
+  
+  # Count
+  count_values = pd.DataFrame(
+      columns=['Records'])
+  for row in list(raw[categorical].columns.values):
+    count_values.loc[row] = [raw[categorical][row].count()]
+  
+  # Missing data
+  missing_data = pd.DataFrame(
+    df[categorical].isnull().sum(),
+    columns=['Missing Values'])
+  
+  # Unique values
+  unique_values = pd.DataFrame(
+      columns=['Unique Values'])
+  for row in list(raw[categorical].columns.values):
+    unique_values.loc[row] = [raw[categorical][row].nunique()]
+
+  # Mode
+  mode_values = pd.DataFrame(
+      columns=['Mode'])
+  for row in list(raw[categorical].columns.values): 
+    mode_values.loc[row] = [raw[categorical][row].mode()[0]]
+    mode = mode_values.loc[row]
+  
+  # Mode frequency
+  listModeFreq = []
+  for row in categorical:
+    mode = raw[row].mode().iat[0]
+    ModeFreq = raw[row].value_counts()[mode]
+    #print(x, mode, raw[x].value_counts()[mode])
+    listModeFreq .append(ModeFreq )
+  listModeFreq  = np.array(listModeFreq )
+    
+  
+  # Create data quality report
+  dq_report_cat = data_types.join(missing_data).join(count_values).join(unique_values).join(mode_values)
+  dq_report_cat['Mode freq.'] = listModeFreq 
+  dq_report_cat['Mode %'] = (dq_report_cat['Mode freq.'] / dq_report_cat['Records']*100) .astype('float')
+  dq_report_cat['Missing %'] = (dq_report_cat['Missing Values'] / len(raw[categorical]) *100)
+
+  # Change order of columns
+  dq_report_cat = dq_report_cat[['Data Type', 'Records', 'Unique Values', 'Missing Values', 'Missing %', 'Mode', 'Mode freq.', 'Mode %']]
+  dq_report_cat[['Missing %', 'Mode %']] =   dq_report_cat[['Missing %'	,  'Mode %']].round(1  )
+
+  # Return report
+  return(dq_report_cat)
+
+
+# Function for creating a descriptive stats report for datetime data
+
+def datetime_dqr(df):
+  
+  # Select numerical columns
+  date = df.select_dtypes(include = ['datetime64[ns]']).columns.tolist()
+  
+  # Data type
+  data_types = pd.DataFrame(
+    df[date].dtypes,
+    columns=['Data Type'])
+  
+  # Missing data
+  missing_data = pd.DataFrame(
+    df[date].isnull().sum(),
+    columns=['Missing Values'])
+  
+  # Unique values
+  unique_values = pd.DataFrame(
+    columns=['Unique Values'])
+  for row in list(df[date].columns.values):
+    unique_values.loc[row] = [df[date][row].nunique()]
+    
+  # Number of records
+  count_values = pd.DataFrame(
+    columns=['Records'])
+  for row in list(df[date].columns.values):
+      count_values.loc[row] = [df[date][row].count()]
+    
+  # Minimum value
+  maximum_values = pd.DataFrame(
+    columns=['Maximum Value'])
+  for row in list(df[date].columns.values):
+      maximum_values.loc[row] = [df[date][row].max()]
+    
+  # Maximum value
+  minimum_values = pd.DataFrame(
+    columns=['Minimum Value'])
+  for row in list(df[date].columns.values):
+      minimum_values.loc[row] = [df[date][row].min()]
+    
+  # Mean value
+  mean_values = pd.DataFrame(
+    columns=['Mean Value'])
+  for row in list(df[date].columns.values):
+      mean_values.loc[row] = [df[date][row].mean()]
+    
+  # First quartile
+  quartile_1 = pd.DataFrame(
+    columns=['1st Quartile'])
+  for row in list(df[date].columns.values):
+      quartile_1.loc[row] = [df[date][row].quantile(0.25)]
+    
+  # Median
+  median = pd.DataFrame(
+  columns=['Median'])
+  for row in list(df[date].columns.values):
+    median.loc[row] = [df[date][row].quantile(0.5)]
+    
+  # Third quartile
+  quartile_3 = pd.DataFrame(
+    columns=['3rd Quartile'])
+  for row in list(df[date].columns.values):
+    quartile_3.loc[row] = [df[date][row].quantile(0.75)]
+    
+  # Join columns 
+  dq_report_date = data_types.join(count_values).join(missing_data).join(unique_values).join(minimum_values).join(maximum_values).join(mean_values).join(quartile_1).join(median).join(quartile_3)
+  
+  # Percentage missing
+  dq_report_date['Missing %'] = (dq_report_date['Missing Values'] / len(df[date]) *100)
+  
+  # Change order of columns
+  dq_report_date = dq_report_date[['Data Type', 'Records', 'Unique Values', 'Missing Values', 'Missing %', 'Mean Value', 'Minimum Value', '1st Quartile', 'Median', '3rd Quartile', 'Maximum Value', ]]
+   
+  # Round   
+  dq_report_date['Mean Value'] = dq_report_date['Mean Value'].dt.date
+  dq_report_date['Missing %'] = dq_report_date['Missing %'].round(1)
+
+    
+  # Return report
+  return(dq_report_date)
+
+
+# Duplicate checker
+
+def duplicate_checker(df):
+  duplicates = df.duplicated(keep = 'first').sum()
+  print("Number of duplicates: {}".format(duplicates))
+  print("Total number of records: {}".format(len(df)))
+  print("Percentage duplicated: {}".format((duplicates/len(df))*100))
+
+
+# Distribution checker
+
+def distribution_checker (data, xlabel):
+  grouped = data.groupby([xlabel, 'status_group'])['id'].count().reset_index()
+  pivot = grouped.pivot_table(index = xlabel, columns = 'status_group', fill_value = 0)
+  mi=pivot.columns
+  mi.tolist()
+  ind = pd.Index([ str(e[1])  for e in mi.tolist()])
+  pivot.columns = ind
+  pivot['nr_pumps'] = pivot['functional'] +pivot['functional needs repair'] +pivot['non functional']
+  pivot['all_pumps'] = pivot['nr_pumps'].sum()
+  pivot['perc_total_pumps'] =((pivot['nr_pumps']/pivot['all_pumps'])*100).round(1)
+  pivot['perc_functional'] = ((pivot['functional']/pivot['nr_pumps'])*100).round(1)
+  pivot['perc_non_functional'] = ((pivot['non functional']/pivot['nr_pumps'])*100).round(1)
+  pivot['perc_functional_needs_repair'] = ((pivot['functional needs repair']/pivot['nr_pumps'])*100).round(1)
+  pivot = pivot.drop(['functional', 'functional needs repair', 'non functional', 'all_pumps'], axis=1)
+  return(pivot)
+
+
+def kdeplotter (dataset, feature, rows, columns):
+  fig, axes = plt.subplots(nrows=rows,ncols=columns, figsize = (30,12))
+  plot = 0
+  for i in range(rows):
+    for j in range(columns):
+       ax = sns.kdeplot(data = dataset, x =feature[plot] , ax = axes[i][j],    shade = True, color  = "black" )
+       plot +=1
+
+
+def countplotter (dataset, feature, rows, columns):
+    fig, axes = plt.subplots(nrows=rows,ncols=columns, figsize = (30,50))
+    plot = 0
+    for i in range(rows):
+        for j in range(columns):
+             ax = sns.countplot(data = dataset, x =feature[plot] , ax = axes[i][j], palette='Set2' )
+             ax.set_title(feature[plot])
+             ax.set_xlabel("")
+             ax.set_xticklabels(ax.get_xticklabels(), rotation = 70, fontsize=9)
+             plot +=1
